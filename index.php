08b059f3b0af40db23c3676e3b54ec41e8be47ff
henock <henoctumonakiese@gmail.com>
2025-12-05 22:01:12 +0100

    web correction

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
       require __DIR__ . DIRECTORY_SEPARATOR .'vendor' . DIRECTORY_SEPARATOR .'autoload.php';
-      require_once 'routes/web.php';
+      require_once __DIR__ . DIRECTORY_SEPARATOR .'routes'. DIRECTORY_SEPARATOR .'web.php';
       Router\Router::matcher();
 ?>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,14 @@
         App\App::view('index');
     });
 
+    Router\Router::get('/archives', function(){
+        App\App::view('archives');
+    });
+    
+    Router\Router::get('/comite', function(){
+        App\App::view('comite');
+    });
+
     Router\Router::get('/test', function(){
        echo "<h1>Royi suce moi la bite</h1>";
     });
